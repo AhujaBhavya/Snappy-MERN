2016814d9c4dcd7035858ddb96a767fbf7184768
AhujaBhavya <126494025+AhujaBhavya@users.noreply.github.com>
2024-12-30 17:25:28 +0530

    Update APIRoutes.js

--- Frontend/src/utils/APIRoutes.js
+++ Frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://snappy-mern-server.vercel.app/api";
+export const host = "http://localhost:5001";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
